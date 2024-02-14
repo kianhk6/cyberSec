7efd5f9078c5415eaad247e11e7aa73441f24bbb
Kian Hosseinkhani <kian_hosseinkhani@sfu.ca>
2024-02-14 00:09:30 -0800

    added initial set up and data gattering

--- part1.R
+++ part1.R
@@ -1,119 +1,30 @@
-getwd()
-setwd("C:/Users/parmi/cmpt318/cyberSec/")
-
-DataDf <- read.table("Group_Assignment_Dataset.txt", header = T, sep = ",")
-# I think you need to install packages before here 
-
-library(ggplot2)
-
-# Assuming 'data' is your data frame and it contains several columns with NAs
-
-# Applying linear interpolation for each column
-data_interp <- DataDf  # Create a copy of the data to store interpolated values
-
-# Loop over each column in the data frame
-for(i in 1:ncol(DataDf)) {
-  # Check if the column has any missing values
-  if(any(is.na(DataDf[[i]]))) {
-    # Get the indices of non-missing values
-    good_data <- na.omit(DataDf[[i]])
-    # Interpolate missing values using the good data
-    data_interp[[i]] <- approx(good_data, xout = seq_along(DataDf[[i]]))$y
-  }
-}
-
-# Now, data_interp contains the data with interpolated values
-options(digits=15)
-data_interp[168502,]
-DataDf[168502,]
-
-#approxm(data1, n, method = "linear")
+# Load necessary libraries
 library(dplyr)
-library(zoo)
-df <- DataDf
-#interpolate missing values in 'sales' column
-df <- df %>%
-   mutate(Global_reactive_power = na.approx(Global_reactive_power))
-df <- df %>%
-  mutate(Voltage  = na.approx(Voltage ))
-df <- df %>%
-   mutate(Global_intensity = na.approx(Global_intensity))
-df <- df %>%
-   mutate(Sub_metering_1  = na.approx(Sub_metering_1 ))
-df <- df %>%
-   mutate(Sub_metering_2  = na.approx(Sub_metering_2 ))
-df <- df %>%
-   mutate(Sub_metering_3  = na.approx(Sub_metering_3 ))
-#view updated data frame
+library(lubridate)
 
-df[168502,]
-data_interp[168502,]
-DataDf[168502,]
-###############################################
-# the date formatted as "DD/MM/YYYY"
+# Assign column names to the dataframe
+names(data) <- c('Date', 'Time', 'Global_active_power', 'Global_reactive_power', 'Voltage', 
+                                     'Global_intensity', 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3')
 
-DataDf$Date <- as.POSIXlt(DataDf$Date, format = "%d/%m/%Y")
 
+# Proceed with the rest of your code for filtering and analysis
+data$Date <- as.POSIXlt(data$Date, format = "%d/%m/%Y")
 # Determine the range of dates for the 6th week in the form "YYYY-MM-DD"
 start_date <- as.POSIXlt("2007-02-05") # start date of the 6th week
 end_date <- as.POSIXlt("2007-02-11") # end date of the 6th week
-
 # Subset the data for the 6th week
-week_data <- DataDf[DataDf$Date >= start_date & DataDf$Date <= end_date, ]
-################################################
-
-ggplot()+layer(data = df, mapping = aes(x=Time, y=Voltage), geom = "point",stat="identity", position = position_identity())
+week_data <- data[data$Date >= start_date & data$Date <= end_date, ]
 
 
-###############################################
 
-ggplot()+
-  layer(data = df, mapping = aes(x=Date, y=Voltage), geom = "point",stat="identity", position = position_identity())
+data$Time <- format(strptime(data$Time, format = "%H:%M:%S"), "%H:%M")
 
-###############################################
+daytime_weekdays <- subset(week_data, as.integer(format(week_data$Date, "%u")) %in% 1:5 & 
+                             Time >= "07:30" & Time <= "17:00")
 
-ggplot()+
-  layer(data = df, mapping = aes(x=Date, y=Voltage), geom = "point",stat="identity", position = position_jitter(width = 0.3, height = 0)) +
-  coord_cartesian() +
-  scale_x_discrete() +
-  scale_y_continuous()
+average_daytime_weekdays <- aggregate(Global_intensity ~ Time, daytime_weekdays, mean)
 
-###############################################
 
-ggplot()+
-  layer(data = df, mapping = aes(x=Date, y=Voltage), geom = "point",stat="identity", position = position_jitter(width = 0.3, height = 0)) +
-  layer(data = df, mapping = aes(x=Date, y=Voltage), geom = "boxplot" ,stat="boxplot", position = position_identity()) +
-  coord_cartesian() +
-  scale_x_discrete() +
-  scale_y_continuous()
 
-###############################################
-
-ggplot()+
-  layer(data = df, mapping = aes(x=Time, y=Voltage, color = Date), geom = "point",stat="identity", position = position_identity()) +
-  coord_cartesian() +
-  scale_x_discrete() +
-  scale_y_continuous() +
-  scale_color_hue()
-
-###############################################
-
-ggplot()+
-  layer(data = df, mapping = aes(x=Time, y=Voltage, color = Global_intensity), geom = "point",stat="identity", position = position_identity()) +
-  coord_cartesian() +
-  scale_x_discrete() +
-  scale_y_continuous() +
-  scale_color_continuous() +
-  facet_wrap(~Date)
-
-###############################################
-
-ggplot()+
-  layer(data = df, mapping = aes(x=Time, y=Voltage, color = Global_intensity), geom = "point",stat="identity", position = position_identity()) +
-  coord_cartesian() +
-  scale_x_discrete() +
-  scale_y_continuous() +
-  scale_color_continuous(low = "#0061ff", high = "#00ffe1", name = "Global_Intensity", guide = guide_colorbar(direction = "vertical")) +
-  facet_wrap(~Date)
-
-###############################################
+weekend_data <- subset(week_data, as.integer(format(week_data$Date, "%u")) %in% c(6, 7))
+average_weekend_daytime <- aggregate(Global_intensity ~ Time, weekend_data, mean)
